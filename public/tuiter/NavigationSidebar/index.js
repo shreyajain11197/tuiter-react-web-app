54fd043cbefef59233e89659ab9c7eab6eff8192
Shreya Bharath Jain <62557509+shreyajain11197@users.noreply.github.com>
2022-10-16 17:45:11 -0400

    A5 challenge changes

--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -1,38 +1,38 @@
-const NavigationSidebar = () => {
+const NavigationSidebar = (active) => {
  return(`
    <div class="list-group">
-     <a class="list-group-item" href="/">
+     <a class="list-group-item disable" href="#">
        <i class="fa-solid fa-t wd-color-font"></i></a>
 
-     <a class="list-group-item" href="/">
+     <a class="list-group-item ${active === 'home' ? 'active': '' }" href="../HomeScreen/index.html">
         <i class="fa fa-home wd-color-font" aria-hidden="true"></i>
             <span class="d-none d-xl-inline d-xxl-inline" >Home</span></a>
 
-     <a class="list-group-item wd-color-font active"  href="#">
+     <a class="list-group-item wd-color-font ${active === 'explore' ? 'active': '' }"  href="../explore/index.html">
         <i class="fa fa-hashtag wd-color-font" aria-hidden="true"></i>
             <span class="d-none d-xl-inline d-xxl-inline">Explore</span></a>
 
-     <a class="list-group-item wd-color-font" href="#">
+     <a class="list-group-item wd-color-font ${active === 'notification' ? 'active': '' }" href="#">
          <i class="fa fa-bell wd-color-font" aria-hidden="true"></i>
             <span class="d-none d-xl-inline d-xxl-inline" >Notifications</span></a>
 
-     <a class="list-group-item wd-color-font" href="#">
+     <a class="list-group-item wd-color-font ${active === 'message' ? 'active': '' }" href="#">
          <i class="fa fa-envelope wd-color-font"  aria-hidden="true"></i>
             <span class="d-none d-xl-inline d-xxl-inline" >Messages</span></a>
 
-     <a class="list-group-item wd-color-font" href="#">
+     <a class="list-group-item wd-color-font ${active === 'bookmarks' ? 'active': '' }" href="#">
          <i class="fa fa-bookmark wd-color-font"  aria-hidden="true"></i>
             <span class="d-none d-xl-inline d-xxl-inline" >Bookmarks</span></a>
 
-     <a class="list-group-item wd-color-font" href="#">
+     <a class="list-group-item wd-color-font ${active === 'lists' ? 'active': '' }" href="#">
          <i class="fa fa-list wd-color-font"  aria-hidden="true"></i>
             <span class="d-none d-xl-inline d-xxl-inline" >Lists</span></a>
 
-     <a class="list-group-item wd-color-font"  href="#">
+     <a class="list-group-item wd-color-font ${active === 'profile' ? 'active': '' }"  href="#">
          <i class="fa fa-user wd-color-font" aria-hidden="true"></i>
             <span class="d-none d-xl-inline d-xxl-inline" >Profile</span></a>
 
-     <a class="list-group-item wd-color-font" href="#">
+     <a class="list-group-item wd-color-font ${active === 'more' ? 'active': '' }" href="#">
          <i class="fa fa-ellipsis-h wd-color-font"  aria-hidden="true"></i>
             <span class="d-none d-xl-inline d-xxl-inline" >More</span></a>
    </div>
@@ -43,5 +43,4 @@ const NavigationSidebar = () => {
    </div>
  `);
 }
-
 export default NavigationSidebar;
