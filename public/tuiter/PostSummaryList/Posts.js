ae583c04cf58f348f4040f9dacad3ebf60ef1a7c
Shreya Bharath Jain <62557509+shreyajain11197@users.noreply.github.com>
2022-10-21 13:54:05 -0400

    A6 tuiter changes + code refactoring

--- public/tuiter/PostSummaryList/Posts.js
+++ public/tuiter/PostSummaryList/Posts.js
@@ -4,14 +4,14 @@ export default [
        userName: 'ReactJS',
        time: '2h',
        title: 'React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs',
-       image: '../../res/React-logo.png'
+       image: '../../res/images/React-logo.png'
    },
    {
        topic: '',
        userName: 'JavaScript',
        time: '1 day',
        title: 'JavaScript is programming language that can run on browsers as well as desktops',
-       image: '../../res/javascript-icon.png',
+       image: '../../res/images/javascript-icon.png',
        tweets: '123K',
    },
    {
@@ -19,7 +19,7 @@ export default [
        userName: '',
        title: 'jQuery',
        time: 'last week',
-       image: '../../res/jquery-icon.png',
+       image: '../../res/images/jquery-icon.png',
        tweets: '122K',
    },
    {
@@ -27,6 +27,6 @@ export default [
        userName: 'NodeJS',
        title: '',
        time: 'last month',
-       image: '../../res/nodejs-icon.png',
+       image: '../../res/images/nodejs-icon.png',
        tweets: '120K',
    },];
