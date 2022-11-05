adc2c002c4dd8a4af2c7f001fb4d23ada1e7cf09
Shreya Bharath Jain <62557509+shreyajain11197@users.noreply.github.com>
2022-11-05 10:55:52 -0400

    A7 lab changes

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import {Routes, Route} from "react-router";
 function App() {
   return (
     <BrowserRouter>
-      <div className="container">
-        <Routes>
-           <Route index element={<Labs/>}/>
-           <Route path="/hello" element={<HelloWorld/>}/>
-           <Route path="/tuiter/*" element={<Tuiter/>}/>
-        </Routes>
-      </div>
+        <div className="container">
+            <Routes>
+                <Route path="/*"
+                       element={<Labs/>}/>
+                <Route path="/hello"
+                       element={<HelloWorld/>}/>
+                <Route path="/tuiter/*"
+                       element={<Tuiter/>}/>
+            </Routes>
+        </div>
     </BrowserRouter>
   );
 }
