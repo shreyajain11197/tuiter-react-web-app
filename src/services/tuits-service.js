48ad9e8cb8797e64ef22455e38978186e610034b
Shreya Bharath Jain <62557509+shreyajain11197@users.noreply.github.com>
2022-11-19 11:55:44 -0500

    tuiter changes a8

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const TUITS_API = 'https://cs5610-tuiter-node-server-ap.herokuapp.com/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
